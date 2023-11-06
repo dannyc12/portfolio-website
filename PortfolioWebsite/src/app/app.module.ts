ed1d5a4e892865e86b6c2584f2389ab6a69f9b38
dannyc12 <danny.clynes12@gmail.com>
2023-11-05 22:22:19 -0600

    add: setup structure for project card modals

--- PortfolioWebsite/src/app/app.module.ts
+++ PortfolioWebsite/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { ProjectCardComponent } from './project-card/project-card.component';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
-import { ProjectCardComponent } from './project-card/project-card.component';
+import { ModalModule }from 'ngx-bootstrap/modal';
+import { ProjectModalComponent } from './project-modal/project-modal.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
@@ -18,15 +21,18 @@ import { ProjectCardComponent } from './project-card/project-card.component';
     HeaderComponent,
     NavComponent,
     HomeComponent,
+    ProjectCardComponent,
     PortfolioComponent,
     ResumeComponent,
     ContactComponent,
-    ProjectCardComponent
+    ProjectModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ModalModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- PortfolioWebsite/src/app/project-card/project-card.component.ts
+++ PortfolioWebsite/src/app/project-card/project-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../models/Project';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { ProjectModalComponent } from '../project-modal/project-modal.component';
 
 @Component({
   selector: 'app-project-card',
@@ -8,4 +10,26 @@ import { Project } from '../models/Project';
 })
 export class ProjectCardComponent {
   @Input() project = {} as Project;
+
+// make bsModalRef optional because the modals won't always be initialized
+  bsModalRef?: BsModalRef;
+
+  // BsModalService provided by ngx-bootstrap
+  constructor(private modalService: BsModalService) {
+    
+  }
+
+  OpenProjectModal() {
+
+    const modalOptions: ModalOptions = {
+      // css styling for our modals
+      class: "modal-lg",
+      initialState: {
+        // takes the project input from the project card component and passes it on to the modal
+        project: this.project
+      }
+    };
+
+    this.bsModalRef = this.modalService.show(ProjectModalComponent, modalOptions);
+  }
 }
