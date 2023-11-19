0d8178f3c96ff3a8ad28b8793f233d611de532b7
Danny <94017099+dannyc12@users.noreply.github.com>
2023-11-19 14:24:53 -0600

    updated projects in project-service, built out resume component

--- PortfolioWebsite/src/app/resume/resume.component.ts
+++ PortfolioWebsite/src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -7,8 +7,22 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent {
+
+  isSkillsOpen: boolean = false;
+  isWorkExperienceOpen: boolean = false;
+  isEducationOpen: boolean = false;
+
   // This sets the title of the site in each the browser tab
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, private renderer: Renderer2) {
     this.titleService.setTitle('Danny Clynes - Resume');
+  }
+
+  downloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/Resume SWE - Danny Clynes (2).pdf');
+    // link.setAttribute('download', 'FastTrack - Danny Clynes version 3.docx.pdf');
+    link.click();
+    link.remove();
   }
 }
